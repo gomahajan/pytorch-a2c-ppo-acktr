16d7e1ac708f2a102669a6c3ebf1b6eb883e7f73
Gaurav Mahajan <gmahajan@eng.ucsd.edu>
2018-08-04 18:05:25 -0700

    Base for linear decision fcn

--- model.py
+++ model.py
@@ -197,9 +197,7 @@ class FactoredMLPBase(nn.Module):
 
         self.decider = nn.Sequential(
                 init_(nn.Linear(num_inputs, hidden_size)),
-                nn.Tanh(),
-                init_(nn.Linear(hidden_size, hidden_size)),
-                nn.Tanh()
+                init_(nn.Linear(hidden_size, hidden_size))
         )
         self.actors = []
 
